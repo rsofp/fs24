9f6bf5741b341ce1f965ad621262c40b8a3cd4a4
developerone2410 <developerone2410@gmail.com>
2024-10-17 10:21:26 +0100

    ajout de la date dynamique du jour

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
     <h1>TP1 GIT</h1>
     <p>Ceci est un fichier servant à tester les commandes de base de git</p>
     <p>Merci de suivre à la lettre les instructions dictées par le document</p>
+    <p>On est le <?php echo date("d/m/Y") ?></p>
 </body>
 </html>
